36b2eed201eb14dc28d8d0b8dfa2e4ce01fe9d0d
kaelixdev-hash <kaelixdev@gmail.com>
2025-12-12 10:19:24 -0300

    Update remake.lua

--- remake.lua
+++ remake.lua
@@ -70,6 +70,7 @@ local redzlib = {
 	Tabs = {},
 	Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/raelhubfunctions/Rael-hub-libary/refs/heads/main/Icons.lua"))()
 }
+
 local ViewportSize = workspace.CurrentCamera.ViewportSize
 local UIScale = ViewportSize.Y / 450
 
@@ -2425,5 +2426,6 @@ end
 end
 
 return redzlib
+
 
 
